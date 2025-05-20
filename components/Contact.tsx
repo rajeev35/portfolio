e81ffdd4cef057b8c60f6423608c4421033b4126
apple <apple@MacBookPro.lan>
2025-05-20 15:33:38 +0530

    gmail fix

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -24,7 +24,7 @@ const social: SocialLink[] = [
   {
     link: "mailto:rajeevkumarq400@gmail.com",
     image: "/mail.svg",
-    handle: "rajeevkumarq400@gmail.com",
+    handle: "gmail.com",
   },
 ];
 
