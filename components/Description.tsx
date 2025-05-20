bf30f81f93d9f0f615bf0a25f4b39312fb318c50
apple <apple@MacBookPro.lan>
2025-05-20 12:57:58 +0530

    discreption updated

--- components/Description.tsx
+++ components/Description.tsx
@@ -9,56 +9,56 @@ const Description = () => {
     <div className="pt-10 lowercase text-sm md:text-base">
       <BlurFade delay={1}>
         <p>
-          My coding journey kicked off when I was 9 years old. I began with
-          block-based coding on websites like Code.org and Scratch. Eventually,
-          I gained interest in web development and used online resources such as
-          YouTube and Udemy to learn HTML, CSS, and JavaScript. A few years
-          later, when I was 11, I got my first MacBook and wanted to learn iOS
-          development, so I self-learned{" "}
+          Hi, I'm Rajeev Kumar, currently working as a Software Engineer at{" "}
           <a
-            href="https://www.swift.org/"
+            href="https://www.raasakarts.com"
+            className="underline transition-all hover:text-blue-500 group"
+            target="_blank"
+          >
+            @Raasakarts 
+            <ArrowUp
+              className="rotate-45 opacity-0 group-hover:opacity-100 w-0 group-hover:w-fit inline transition-all"
+              color="#3b82f6"
+              width={20}
+            />
+          </a>
+          , a Shark Tank India-funded startup. I specialize in full-stack
+          development with a focus on the MERN stack and have a strong passion
+          for emerging technologies like{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Blockchain"
             className="underline transition-all hover:text-[#005BE7] group"
             target="_blank"
           >
-            Swift
+            blockchain
             <ArrowUp
               className="rotate-45 opacity-0 group-hover:opacity-100 w-0 group-hover:w-fit inline transition-all"
               color="#005BE7"
               width={20}
             />
           </a>{" "}
-          and SwiftUI. Fast forward to when I was about to turn 13, I found out
-          about the{" "}
+          and{" "}
           <a
-            href="https://developer.apple.com/swift-student-challenge/"
-            className="underline transition-all hover:text-blue-500 group"
+            href="https://en.wikipedia.org/wiki/DevOps"
+            className="underline transition-all hover:text-[#005BE7] group"
             target="_blank"
           >
-            Swift Student Challenge
+            DevOps
             <ArrowUp
               className="rotate-45 opacity-0 group-hover:opacity-100 w-0 group-hover:w-fit inline transition-all"
-              color="#3b82f6"
+              color="#005BE7"
               width={20}
             />
-          </a>{" "}
-          and I started developing an offline app for it. When I submitted it, I
-          thought I had a very slim chance of being one of the 350 winners, but
-          I made it! I even got an offer from Apple to attend{" "}
-          <a
-            href="https://developer.apple.com/wwdc24/"
-            className="underline transition-all hover:text-blue-500 group"
-            target="_blank"
-          >
-            WWDC24
-            <ArrowUp
-              className="rotate-45 opacity-0 group-hover:opacity-100 w-0 group-hover:w-fit inline transition-all"
-              color="#3b82f6"
-              width={20}
-            />
-          </a>{" "}
-          in-person. It was a very unique experience in Cupertino, meeting many
-          new people. Right now, I am continuing to build more projects and
-          learn new skills such as Next.js and Python.
+          </a>
+          .
+         
+          Currently, I’m exploring how technology can transform industries and
+          improve systems. I believe in the power of collaboration and am always
+          looking for opportunities to connect with fellow professionals to
+          learn and grow together.
+          <br />
+          <br />
+          🚀 Open to exciting opportunities and conversations in the tech space!
         </p>
       </BlurFade>
     </div>
