401b85e5fed744afc578538ff8d66fa3b6b1d7f7
apple <apple@MacBookPro.lan>
2025-05-23 00:27:10 +0530

    svg fix

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -71,10 +71,11 @@ export default function Skills() {
           </g>
         </svg>
         
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
+        
+        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
           <circle cx="50" cy="50" r="22" fill="#6f42c1"/>
           <text x="50%" y="50%" text-anchor="middle" dy=".3em" font-size="20" fill="#fff" font-family="Arial, sans-serif">PHP</text>
-        </svg>
+        </svg> */}
         
 
         {/* <svg
